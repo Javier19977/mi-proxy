3b9f13ef62ff905f2625998887f1b72f5e5e1d06
Javier19977 <146967783+Javier19977@users.noreply.github.com>
2026-05-11 06:41:09 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,12 @@
 
+export default async function handler(req, res) {
+  const GOOGLE_URL = "https://script.google.com/macros/s/AKfycbyFMN-lSmsFBOFkmhL76lHG6KcF7JtSnmErAuVfjWNnfIKQxkdr5UNCX1VyBgIeWKeAiw/exec";
+  
+  const response = await fetch(GOOGLE_URL, {
+    method: req.method,
+    body: req.method === "POST" ? JSON.stringify(req.body) : undefined,
+  });
+  
+  const data = await response.json();
+  res.status(200).json(data);
+}
